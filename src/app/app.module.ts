00daeba66bd5e1859c93b488c7a68e5e67ac221b
oleg <g4Cu3H9s>
2018-12-15 01:19:09 +0300

    Merge branch 'master' of /home/oleg/my-app with conflicts.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,41 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {AppFootballComponent} from './football/app.football';
+import {HttpClientModule} from '@angular/common/http';
+import {TeamService} from './service/team.service';
+import {FormsModule} from '@angular/forms';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatTableModule, MatSortModule, MatPaginatorModule, MatFormFieldModule} from '@angular/material';
+import {MatDialogModule, MatDialog, MatInputModule} from '@angular/material';
+import {DialogComponent, DialogOverviewExampleDialogComponent} from './dialog/dialog.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppFootballComponent,
+    DialogComponent,
+    DialogOverviewExampleDialogComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatSortModule,
+    MatPaginatorModule,
+    MatDialogModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  entryComponents: [DialogOverviewExampleDialogComponent],
+  providers: [TeamService, MatDialog],
+  bootstrap: [AppFootballComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
